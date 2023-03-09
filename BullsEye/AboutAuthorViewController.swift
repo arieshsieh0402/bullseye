bf0c7fd8f7210b6fe0389af9b0502ca4f2f82df5
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2023-03-09 17:16:21 +0800

    Finished all features

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Hsieh-Te Hsieh on 2023/3/9.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true)
+    }
+    
+
+}
